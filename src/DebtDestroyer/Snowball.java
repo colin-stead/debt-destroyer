668a6172be0719bb0964074cd449b240370ccce6
Ryan Bramich <ryan.bramich@outlook.com>
2023-05-29 21:43:58 -0400

    updated snowball class to copy data from debt object array list to string multidim array to preserve data integrity when we start simulating paying down the loan to compute payoff times.

--- src/DebtDestroyer/Snowball.java
+++ src/DebtDestroyer/Snowball.java
@@ -14,27 +14,53 @@ import java.util.ArrayList;
  * and so on and so forth until all debt has been paid off.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 public class Snowball {
-ArrayList<Debt> debtList = new ArrayList<Debt>();
+//ArrayList<Debt> debtList = new ArrayList<Debt>();
+String[][] debtCopy;
+
 //Accepts arraylist of users debts income and expenses or we can do another object of of the user with thier expenses and income.
 Snowball(ArrayList<Debt> debtlist){
-this.debtList= debtlist;
+//this.debtList= debtlist;
+copyDebt(debtlist);
 }
 //TODO this is a place holder. going testing abilty to get name from this debt list from snowball class.
 public void getDebtName(int location){
-    System.out.println(debtList.get(location).debtName);
+    //System.out.println(debtList.get(location).debtName);
 }
 
 //TODO create method to get all of the total amounts that are owed.
-public void getOwedAmounts(){
-    for(int i=0; i<debtList.size();i++){
-        System.out.println("Amount owed on " + debtList.get(i).getDebtName() + " is " + debtList.get(i).getTotalAmountOwed());
+// public void getOwedAmounts(){
+//     for(int i=0; i<debtList.size();i++){
+//         System.out.println("Amount owed on " + debtList.get(i).getDebtName() + " is " + debtList.get(i).getTotalAmountOwed());
+//     }
+// }
+//Copy the debt object into a String array so that we can minupulate the data with out messing with the integrity of the data since its pass by referernece.
+private void copyDebt(ArrayList<Debt> debtList){
+    this.debtCopy = new String[debtList.size()][4]; 
+    for(int i=0; i<debtList.size(); i++){
+        debtCopy[i][0] = debtList.get(i).debtName; // 0 = lender name
+        debtCopy[i][1] = String.valueOf(debtList.get(i).totalAmountOwed); // 1 = total amount owed
+        debtCopy[i][2] = String.valueOf(debtList.get(i).minimumMonthlyPayment); // 2 = minumim montly payment amount
+        debtCopy[i][3] = String.valueOf(debtList.get(i).APR); // 3 = Annual Precentage rate.
+    }
+
+}
+
+public void printMatrix(){
+    //copyDebt();
+    for (int i=0; i<debtCopy.length; i++){
+        for(int j=0; j<4; j++){
+            if(j==3){
+            System.out.print(debtCopy[i][j]+ " ");}
+            else
+            System.out.print(debtCopy[i][j]+ ", ");
+        }
+        System.out.println();
     }
 }
 
-
 //TODO sort all of the total amounts owed from lowes to greates.
 
-//TODO calc payoff timframe based on paying min on non-lowest loan and paying max afforadable on lowest owed loan.
+//TODO calc payoff timeframe based on paying min on non-lowest loan and paying max afforadable on lowest owed loan.
 }
 
 
--- src/DebtDestroyer/TestSnow.java
+++ src/DebtDestroyer/TestSnow.java
@@ -6,11 +6,14 @@ public class TestSnow {
     public static void main(String[] args) {
         Debt liberty = new Debt("Liberty Mutial", 15000, 25, 2.5);
         Debt American = new Debt("American Express", 10000, 100, 5);
-    ArrayList<Debt> debList = new ArrayList<Debt>();
+        Debt Citizens = new Debt("Citizens", 50000, 500, 1.5);
+        ArrayList<Debt> debList = new ArrayList<Debt>();
     debList.add(liberty);
     debList.add(American);
+    debList.add(Citizens);
     Snowball snow = new Snowball(debList);
-    snow.getOwedAmounts();
+    //snow.getOwedAmounts();
+    snow.printMatrix();
 
 
     }  
