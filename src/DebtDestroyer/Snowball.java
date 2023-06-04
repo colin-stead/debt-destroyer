065e8e34dc818d4f416e585df8904029e33d793e
Ryan Bramich <ryan.bramich@outlook.com>
2023-06-04 16:57:28 -0400

    completed initial implimentaiton of snowball algoritm. still have to clean up the paying the remaing funds foward and such.

--- src/DebtDestroyer/Snowball.java
+++ src/DebtDestroyer/Snowball.java
@@ -28,8 +28,7 @@ public class Snowball {
         amountPay = 475.00;
         sortDebtList();
         copyDebt(this.debts);
-        addAPR();
-
+        calculatePayoff();
     }
 
     // TODO this is a place holder. going testing abilty to get name from this debt
@@ -54,11 +53,12 @@ public class Snowball {
             debtCopy[i][0] = debtList.get(i).debtName; // 0 = lender name
             debtCopy[i][1] = String.valueOf(debtList.get(i).totalAmountOwed); // 1 = total amount owed
             debtCopy[i][2] = String.valueOf(debtList.get(i).minimumMonthlyPayment); // 2 = minumim montly payment amount
-            debtCopy[i][3] = String.valueOf((debtList.get(i).APR/100)); // 3 = Annual Precentage rate.
+            debtCopy[i][3] = String.valueOf((debtList.get(i).APR / 100)); // 3 = Annual Precentage rate.
         }
 
     }
-    //Method used to print the String array to see the lender information
+
+    // Method used to print the String array to see the lender information
     public void printMatrix() {
         // copyDebt();
         for (int i = 0; i < debtCopy.length; i++) {
@@ -95,47 +95,76 @@ public class Snowball {
     // for each month a minimum payment will need to be applied to the loan so that
     // there is not a late fee applied.
     private void calculatePayoff() {
-        // set inital total amount owed value loan are sorted to the lowest loan is in the first location.
-        this.lowestLoanInitialAmt = Double.valueOf(this.debtCopy[0][1]); 
-            if(amountPay>Double.valueOf(debtCopy[0][1])){
-                double temp =  amountPay - Double.valueOf(debtCopy[0][1]);
-                this.debtCopy[0][1] = String.valueOf(0.0);
-                System.out.println("Amount left to pay to the next loan is: " + temp);
-        }else{
-        this.debtCopy[0][1] = String.valueOf(Double.valueOf(debtCopy[0][1]) - amountPay);}
-        System.out.println("Amount after monthly pay off: " + this.debtCopy[0][1]);
-        
+        // set inital total amount owed value loan are sorted to the lowest loan is in
+        // the first location.
+        this.lowestLoanInitialAmt = Double.valueOf(this.debtCopy[0][1]);
+        boolean paidInFull = false;
+        int lowest = 0; // lowest loan is in the first[0] location
+        int month =1;
+        double temp =0.0;
+        while (paidInFull == false) {
+            if (amountPay > Double.valueOf(debtCopy[lowest][1])) {
+                 temp = amountPay - Double.valueOf(debtCopy[lowest][1]);
+                this.debtCopy[lowest][1] = String.valueOf(0.0);
+                System.out.println("Month " + month  + "\nLender - "  + debtCopy[lowest][0] + " is paid off\nAmount left to pay to the next loan is: " + temp);
+                this.amountPay+= Double.valueOf(debtCopy[lowest][2]);
+                System.out.println("New Monthly amount to pay: " + amountPay);
+                if(lowest+1<debtCopy.length){
+                lowest++;
+                System.out.println("lowest now = " + lowest + " New Loan being paid off is " + debtCopy[lowest][0]);
+                }
+            } else {
+                this.debtCopy[lowest][1] = String.valueOf(Double.valueOf(debtCopy[lowest][1]) - amountPay);
+                System.out.println("Month " + month  + "\nAmount after monthly pay off: " + this.debtCopy[lowest][1]);
+            }
+            //Pay the min values payments on all other loans so there are no extra finance charges.
+            if(lowest+1 <debtCopy.length){//if this is false then we are working on the last loan and we dont need to do this.
+            for(int i=lowest+1; i<debtCopy.length; i++){
+                //If there is a positive varance left over from the previous loan pay off apply it to the first loan minimum pay off.
+                if(temp>0.0){
+                    debtCopy[i][1] = String.valueOf(Double.valueOf(debtCopy[i][1])-(Double.valueOf(debtCopy[i][2]) + temp));
+                    temp = 0.0;
+                }
+                else{
+                    debtCopy[i][1] = String.valueOf(Double.valueOf(debtCopy[i][1])-Double.valueOf(debtCopy[i][2]));
+                }
+            }
+        }
+            addAPR(lowest);
+            month++;
+
+            for (int i = lowest; i < debtCopy.length; i++) {
+                if (Double.valueOf(debtCopy[i][1]) == 0.0) {
+                    paidInFull = true;
+                } else
+                    paidInFull = false;
+            }
+            // if(month>150){
+            //     paidInFull = true;
+            // }
+
+        }
+        System.out.println("Total interest paid through life of loan pay off was: " + totalInterest);
 
     }
-    private void addAPR(){
-        double currentInterest=0.0;
-        int month = 1;
-        int lowest = 0; //lowest loan is in the first[0] location
-        boolean paidInFull = false;
-        // while(Double.valueOf(debtCopy[lowest][1])>0.0){//make this a boolean value that will check that all loans are paid off.
-        while(paidInFull==false){//make this a boolean value that will check that all loans are paid off.
-        System.out.println("Month " + month);
-        for(int i=0; i<debtCopy.length; i++){ //for loop to cycle through all of the users loans. i may become lowests loan and we can propaly increnemt that var once its paid off so we dont have to keep checking a known paid off loan. 
-            if(Double.valueOf(debtCopy[i][1]) !=0){
-                calculatePayoff();
+
+    private void addAPR(int lowest) {
+        double currentInterest = 0.0;
+        // while(Double.valueOf(debtCopy[lowest][1])>0.0){//make this a boolean value
+        // that will check that all loans are paid off.
+        for (int i = lowest; i < debtCopy.length; i++) { // for loop to cycle through all of the users loans. i may
+                                                         // become lowests loan and we can propaly increnemt that var
+                                                         // once its paid off so we dont have to keep checking a known
+                                                         // paid off loan.
+            if (Double.valueOf(debtCopy[i][1]) != 0) {
                 currentInterest = (Double.valueOf(debtCopy[i][1]) * Double.valueOf(debtCopy[i][3]));
-                System.out.println(currentInterest);
                 this.totalInterest = this.totalInterest + currentInterest;
                 debtCopy[i][1] = String.valueOf(currentInterest + Double.valueOf(debtCopy[i][1]));
                 System.out.println("Amount after interst charges: " + debtCopy[i][1]);
             }
         }
         System.out.println();
-        month++;
-        //Check that all loans are paid off
-        for(int i=lowest; i<debtCopy.length; i++){
-            if(Double.valueOf(debtCopy[i][1])==0.0){
-                paidInFull = true;
-            }else
-            paidInFull = false;
-        }
-    }
-    System.out.println("Total interest paid through life of loan pay off was: " + totalInterest);
+        
     }
 
     // TODO Add method to check/catch checking that the expenses plus all of the
--- src/DebtDestroyer/TestSnow.java
+++ src/DebtDestroyer/TestSnow.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class TestSnow {
     public static void main(String[] args) {
         // Debt Citizens = new Debt("Citizens", 50000, 500, 1.5);
-        // Debt liberty = new Debt("Liberty Mutial", 15000, 25, 2.5);
-        Debt American = new Debt("American Express", 10000, 100, 2);
+        Debt liberty = new Debt("Liberty Mutial", 5000, 25, .7);
+        Debt American = new Debt("American Express", 10000, 100, 1.5);
         ArrayList<Debt> debList = new ArrayList<Debt>();
-    // debList.add(liberty);
+    debList.add(liberty);
     debList.add(American);
     // debList.add(Citizens);
     Snowball snow = new Snowball(debList);
