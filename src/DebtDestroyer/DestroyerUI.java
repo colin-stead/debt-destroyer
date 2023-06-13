0352346fee79f776a0e16779afc708909273ff17
Colin <colin.stead49@gmail.com>
2023-06-13 02:41:39 +0000

    Front End inputs

--- src/DebtDestroyer/DestroyerUI.java
+++ src/DebtDestroyer/DestroyerUI.java
@@ -14,7 +14,7 @@ public class DestroyerUI extends Application {
     public void start(Stage stage) {
     	try {
 			Parent root = FXMLLoader.load(getClass().getResource("debtdestroyer.fxml"));
-	        Scene scene = new Scene(root, 950, 800);
+	        Scene scene = new Scene(root, 775, 600);
 	        stage.setTitle("Debt Destroyer");
 	        stage.setScene(scene);
 	        stage.show();
